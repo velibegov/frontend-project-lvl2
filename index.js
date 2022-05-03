4351c69e45dbc2e9cf63973987507c099ae84db0
RVelibegov <velibegov@mail.ru>
2022-05-03 15:44:42 +0300

    тест хекслета не видит модуль

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import genDiff from "./src/gendiff.js";
+import genDiff from "./src/GenDiff.js";
 
 const gendiff = (path1, path2, format = "stylish") => genDiff(path1, path2, format);
 export default gendiff;
