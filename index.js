8ebbcf453f0178d1504c185eb8add72e39265ee6
RVelibegov <velibegov@mail.ru>
2022-05-03 15:55:53 +0300

    не видит модуль

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import genDiff from "./src/GenDiff.js";
 
-const app = (path1, path2, format = "stylish") => genDiff(path1, path2, format);
+const app = (path1, path2, format = "stylish") => {
+  return genDiff(path1, path2, format);
+}
 export default app;
